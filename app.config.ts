82fc3aa70c511a05115f83e5c7b737ca3a71383e
gabriel.projects90 <gabriel.projects90@proton.me>
2023-12-10 13:09:16 +0200

    fix merge

--- app.config.ts
+++ app.config.ts
@@ -1,15 +1,3 @@
 export default defineAppConfig({
-  contact: {
-      linkedin: undefined,
-      email: 'gabriel.projects90@proton.me',
-      github: undefined,
-      pdf: undefined,
-      phone: undefined
-  },
-
-  parent: {
-    phone: '0761030884',
-    email: 'gabriel.projects90@proton.me',
-    github: 'https://github.com/gabiap90'
-  }
+    
 })
